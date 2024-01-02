c2c26f72764352912d57af548f82da3efc65d359
MY ASUS <safirasafira.aulia123@mail.com>
2024-01-02 17:16:45 +0700

    baru2

--- members/models.py
+++ members/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    NAMA = models.CharField(max_length=50)
+
+    def _str_(self):
+        return f"{self.NAMA}"
